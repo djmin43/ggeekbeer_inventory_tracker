3aad6d688d4f969b36062ec4750186e0b971c7e6
jay <dj.min43@gmail.com>
2021-04-25 16:43:42 +0900

    api call

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -1,5 +1,10 @@
 "use strict";
-// Inventory Controllers
+Object.defineProperty(exports, "__esModule", { value: true });
+const Brew = require('../db/models/brew.js');
+const Event = require('../db/models/event.js');
+const Inventory = require('../db/models/inventory.js');
+const Purchase = require('../db/models/purchase.js');
+const User = require('../db/models/user.js');
 module.exports.info_get = (req, res) => {
     res.json({ msg: 'information get page' });
 };
