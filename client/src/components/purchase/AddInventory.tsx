50f1b8fa89e9a9a85e6e264d43290c014cdea860
jay <dj.min43@gmail.com>
2021-05-03 23:31:28 +0900

    plans for purchase api. must finish by tomrrow.

--- client/src/components/purchase/AddInventory.tsx
+++ client/src/components/purchase/AddInventory.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const AddInventory = ({today, inventoryInfo}: any) => {
+
+// This is not a new purchase. This happens when the purchase is repeated. User chooses the existing purchase order, then create a patch request.
+
     return (
         <div>
             
--- client/src/components/purchase/NewPurchase.tsx
+++ client/src/components/purchase/NewPurchase.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const NewPurchase = ({today, inventoryInfo}: any) => {
+
+// This is totally a new purchase, just like 'addBrew.tsx'
+
     return (
         <div>
             
