6e38c6e438a9d36a074b25f9aa116c539436faa3
jay <dj.min43@gmail.com>
2021-05-16 15:30:55 +0900

    inventory detail page ready

--- client/src/components/inventory/InventoryDetail.tsx
+++ client/src/components/inventory/InventoryDetail.tsx
@@ -1,37 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { TodayContext } from '../../DataContext'
 
 const InventoryDetail = ({inventoryInfo}: any) => {
 
-    const [selectDetail, setSelectDetail] = useState<any>([
+    const today = useContext(TodayContext)
 
-    ])
+    const defaultInventory: any = {
+        inventory_name: '',
+        inventory_type: '',
+        inventory_amount: '',
+        expiration_date: today,
+        inventory_desc: '',
+        events:[]
+    }
+
+    const [inventory, setInventory] = useState<any>(defaultInventory)
 
     const handleChange = async (e: any) => {
         e.preventDefault()
-        await setSelectDetail(inventoryInfo[e.target.value])
+        await setInventory(inventoryInfo[e.target.value])
     }
 
     return (
         <div>
             <h1>재료정보</h1>
             <select onChange={handleChange}>
-                <option>원하시는 재고를 고르세요.</option>
+                <option value="0">원하시는 재고를 고르세요.</option>
                 {inventoryInfo.map((item:any, index: number) => 
                     <option key={index} value={index}>{item.inventory_name}</option>
                 )}
             </select>
 
+            {/* TABLES for Selected Inventory */}
             <div>
-                <p>재료이름: </p>{selectDetail.inventory_name}
-                <p>재료내용</p>{selectDetail.events.map((item:any) =>
-                <div> 
-                <p>{item.event_type}</p>
-                <p>{item.event_amount}</p>
-                </div>
-                )}
+                {/* TABLE #1 - Selected Inventory */}
+                <table>
+                    <thead>
+                        <tr>
+                            <th>재료이름: </th>
+                            <th>재료양: </th>
+                            <th>유통기한: </th>
+                            <th>재료설명: </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{inventory.inventory_name}</td>
+                            <td>{inventory.inventory_amount}</td>
+                            <td>{inventory.expiration_date}</td>
+                            <td>{inventory.inventory_desc}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                
+                {/* TABLE#2 - events */}
+                <table>
+                    <thead>
+                        <tr>
+                            <th>이벤트타입</th>
+                            <th>양</th>
+                            <th>날짜</th>
+                            <th>비고</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {inventory.events.map((item:any, index:number) => 
+                        <tr key={index}>
+                            <td>{item.event_type}</td>
+                            <td>{item.event_amount}</td>
+                            <td>{item.event_date}</td>
+                            <td>{item.event_desc}</td>
+                        </tr>
+                        )}
+                    </tbody>
+                </table>
+    
             </div>
-
-
         </div>
     )
 }
