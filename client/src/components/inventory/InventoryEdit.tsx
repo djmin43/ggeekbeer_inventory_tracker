411b15c2e07b7d8296277b6cec485d36a2113c0c
jay <dj.min43@gmail.com>
2021-05-16 17:10:49 +0900

    label for edit done.

--- client/src/components/inventory/InventoryEdit.tsx
+++ client/src/components/inventory/InventoryEdit.tsx
@@ -22,8 +22,23 @@ const InventoryEdit = ({selectIndex, setEditing}: any) => {
     return (
         <div>
             <form>
-                <label>
+                <label>재고이름:
+                    <input name="inventory_name" type="text" value={editInventory.inventory_name} onChange={handleChange}></input>
+                </label>
+                <label>양:
                     <input name="inventory_amount" type="number" value={editInventory.inventory_amount} onChange={handleChange}></input>
+                </label>
+                <label>타입:
+                    <input name="inventory_type" type="text" value={editInventory.inventory_type} onChange={handleChange}></input>
+                </label>
+                <label>유통기한
+                    <input name="expiration_date" type="date" value={editInventory.expiration_date} onChange={handleChange}></input>
+                </label>
+                <label>설명
+                    <input name="inventory_desc" type="text" value={editInventory.inventory_desc} onChange={handleChange}></input>
+                </label>
+                <label>입고날짜
+                    <input name="import_date" type="date" value={editInventory.import_date} onChange={handleChange}></input>
                 </label>
             </form>
         </div>
