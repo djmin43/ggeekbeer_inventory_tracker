c82c579befffd1dcf5047f605b0d8463d692209b
jay <dj.min43@gmail.com>
2021-05-28 18:55:10 +0900

    database rollback

--- knexfile.js
+++ knexfile.js
@@ -6,16 +6,14 @@ dotenv.config({ path: './config/.env'});
 module.exports = {
 
     development: {
-      client: "pg",
-      connection: 
-      {
+      client:  'postgresql',
+      connection: {
         host : process.env.DB_HOST,
         user : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
         database : process.env.DB_NAME,
-        rejectUnauthorized: true,
-        ssl: true      
       },
+      ssl: { rejectUnauthorized: false },
       pool: {
         min: 1,
         max: 10
